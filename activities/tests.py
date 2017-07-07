eff986698a7517114716824d55a43e11a6b770db
Jean Bauer <jabauer@uchicago.edu>
2017-07-07 16:41:16 -0400

    Tested assignment_types view

--- activities/tests.py
+++ activities/tests.py
@@ -5,8 +5,11 @@ when you run "manage.py test".
 Replace this with more appropriate tests for your application.
 """
 
+from django.core.urlresolvers import reverse
 from django.test import TestCase
+
 from .models import AssignmentType
+
 
 class TestAssignmentType(TestCase):
 	fixtures = ['assignment_type.json']
@@ -14,3 +17,24 @@ class TestAssignmentType(TestCase):
 	def test_str(self):
 		atype = AssignmentType.objects.first()
 		assert unicode(atype) == atype.name
+
+
+class TestActivitiesViews(TestCase):
+	fixtures = ['assignment_type.json']
+
+	def test_assignment_type_list(self):
+		atype_list_url = reverse('assignment_type_list')
+		response = self.client.get(atype_list_url)
+		assert response.status_code == 200
+		for atype in AssignmentType.objects.all():
+			self.assertContains(response, atype.name, 
+				msg_prefix='assignment type list should include %s' % atype.name)
+			self.assertContains(response, reverse('assignment_type_detail', args=[atype.id]), 
+				msg_prefix='assignment type list should include %s' % atype.id)
+		#print response.content
+		self.assertContains(response, '%d total' % AssignmentType.objects.count())
+
+	def test_assignment_type_detail(self):
+		atype_detail_url = reverse('assignment_type_detail', args=[1])
+		response = self.client.get(atype_detail_url)
+		assert response.status_code == 404
