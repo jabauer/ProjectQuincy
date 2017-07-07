a8a98acd4b225f5635184713f9240059c9c0d526
Jean Bauer <jabauer@uchicago.edu>
2017-07-07 15:41:05 -0400

    Updated travis settings

--- ci/testsettings.py
+++ ci/testsettings.py
@@ -5,13 +5,34 @@
 # have to be manually restarted because changes will not be noticed
 # immediately.
 
-# DEBUG = False
+DEBUG = False
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#     }
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        # Django needs to make databases in the test mysql server
+        'NAME': 'travispq',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': '127.0.0.1',
+        'OPTIONS': {
+            # In each case, we want strict mode on to catch truncation issues
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
+            # Load the socket info from .my.cnf in the travis user
+            'read_default_file': '~travis/.my.cnf'
+        },
+        'PORT': '',
+        'TEST': {
+                # We also want the test databse to for utf8 and the general
+                # collation to keep case sensitive unicode searches working
+                # as we would expect on production
+                'CHARSET': 'utf8',
+                'COLLATION': 'utf8_general_ci',
+                'OPTIONS': {
+                    'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
+                },
+            },
+    },
+}
 
-
+# secret key added as a travis build step
